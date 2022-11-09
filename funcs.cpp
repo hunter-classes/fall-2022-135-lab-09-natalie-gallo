f2ece7d29704ba08009e2f33bb696c4744cb03bb
Natalie Gallo <nataliegallo@Natalies-Air.fios-router.home>
2022-11-09 15:05:21 -0500

    moving on to task c

--- funcs.cpp
+++ funcs.cpp
@@ -1,5 +1,29 @@
 #include <iostream>
+#include <cmath>
 #include "funcs.h"
+#include "coord3d.h"
 
 // add functions here
 
+//TASK A
+
+double length(Coord3D *p){
+  double x = p->x;
+  double y = p->y;
+  double z = p->z;
+  return sqrt(pow(x, 2) + pow(y, 2) + pow(z, 2));
+}
+
+//TASK B
+
+Coord3D * fartherFromOrigin(Coord3D *p1, Coord3D *p2){
+  double point1 = length(p1);
+  double point2 = length(p2);
+  if (point1 > point2){
+    return p1;
+  } else {
+    return p2;
+  }
+}
+
+//TASK C
--- main.cpp
+++ main.cpp
@@ -1,9 +1,31 @@
 #include <iostream>
-
-
+#include <cmath>
 #include "funcs.h"
+#include "coord3d.h"
 
 int main()
 {
+
+  //TASK A
+  Coord3D pointP = {10, 20, 30};
+  Coord3D *p = &pointP; //PERSONAL TESTS
+  cout << p->x << endl; 
+  cout << p->y << endl;
+  cout << p->z << endl;
+  cout << length(&pointP) << endl; //MUST INCLUDE IN TESTS.CPP
+
+  //TASK B
+  Coord3D pointP = {10, 20, 30};
+  Coord3D pointQ = {-20, 21, -22};
+
+  cout << "Address of P: " << &pointP << endl;
+  cout << "Address of Q: " << &pointQ << endl << endl;
+
+  Coord3D * ans = fartherFromOrigin(&pointP, &pointQ); //meant to return address right??
+
+  cout << "ans = " << ans << endl; // So which point is farther?
+
+  //TASK C
+  
   return 0;
 }
